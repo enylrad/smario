25a89e73c8fd168f1250fa747783792e2fe13f53
David Pérez <enylrad@gmail.com>
2016-12-27 11:51:09 +0100

    Initial commit

---
+++ desktop/src/es/enylrad/smario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.enylrad.smario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.enylrad.smario.MarioBrosMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBrosMain(), config);
+	}
+}
